91474e62bbb9da1c248877c8e2800e28d600b7eb
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2024-01-11 02:08:56 +0530

    Adding multiselect files

---
+++ projects/angular-multiselect-plus/public-api.ts
@@ -0,0 +1,6 @@
+export { AngularMultiSelect } from './multiselect.component';
+export { ClickOutsideDirective,ScrollDirective,styleDirective,setPosition } from './clickOutside';
+export { ListFilterPipe } from './list-filter';
+export { Item } from './menu-item';
+export { TemplateRenderer,Badge,Search,CIcon } from './menu-item';
+export { AngularMultiSelectModule } from './multiselect.component';
